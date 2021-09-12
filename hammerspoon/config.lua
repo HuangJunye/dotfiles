a988bf9e699c79bd59e34a2efe9100b313cb30e0
Junye Huang <h.jun.ye@gmail.com>
2021-09-13 00:41:01 +0800

    add ssh and hammerspoon

---
+++ hammerspoon/config.lua
@@ -0,0 +1,9 @@
+MACBOOK_MONITOR = 'Built-in Retina Display'
+
+HOME_LEFT_MONITOR = 'HP ZR2740w'
+HOME_RIGHT_MONITOR = 'DELL P2715Q'
+
+IDEs = {
+  'Emacs',
+  'DrRacket',
+}
---
+++ hammerspoon/screen.lua
@@ -0,0 +1,2 @@
+hs.grid.setGrid('5x3', HOME_RIGHT_MONITOR)
+hs.grid.setGrid('2x4', HOME_LEFT_MONITOR)
